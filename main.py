3235f3e4b7b1562b7376c4e3db45d44e8ec34f93
LastWeekNextDay <56513101+LastWeekNextDay@users.noreply.github.com>
2023-11-08 21:24:16 +0200

    next

--- main.py
+++ main.py
@@ -8,8 +8,8 @@ if __name__ == '__main__':
     data = pd.read_csv(file_path)
 
     # 'Avg_players' and 'Peak_Players'
-    X_set = data['Avg_players'].values
-    Y_set = data['Peak_Players'].values
+    X_set = data['Peak_Players'].values
+    Y_set = data['Avg_players'].values
     points_list = [[x, y] for x, y in zip(X_set, Y_set)]
 
     Algorithms.knn_rand(points_list)
